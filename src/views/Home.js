cfbe17695c5df13637263758216db6beb8b2982a
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-03-17 18:09:29 +0700

    eight commit About

--- src/views/Home.js
+++ src/views/Home.js
@@ -97,7 +97,7 @@ export default class Home extends Component {
   render(){
     return(
       <div id="home">
-        {/* <NavigationBar /> */}
+        <NavigationBar />
 
         <HeadComponent bg={HeadBackground} text="An Epicurean Journey of the Ages" />
         

075ce72bdbe642ffe5e448e34cc0dafc3b59e553
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-03-22 16:15:05 +0700

    eleventh commit

--- src/views/About.js
+++ src/views/About.js
@@ -46,6 +46,9 @@ import { layoutGenerator } from 'react-break';
 
 import { dataAboutPage } from '../services/dummyData'
 
+import MobileCardTextSecondary from '../components/base_components/MobileCard/CardText/MobileCardTextSecondary'
+import { MobileTitle } from '../components/base_components/MobileDivider/Title'
+
 const layout = layoutGenerator({
   mobile: 0,
   tablet: 768,
@@ -94,122 +97,169 @@ export default class Home extends Component {
       <div id="about">
         <NavigationBar />
         <HeadComponent bg={HeadBackground} text="About" />
+        <OnDesktop>
+          <section>
+              <div className="container">
+                  <TitlePage name="ABOUT" marginLeft="91px" />
+              </div>
+          </section>
+          <section>
+              <div className="container">
+                  <div style={{marginTop : "38px"}}></div>
+                  <SmallArticle 
+                      align="center"
+                      caption = "OUR STORY"
+                      title ={dataAboutPage[0].article.title}
+                      firstParagraph ={dataAboutPage[0].article.firstParagraph}
+                      image={dataAboutPage[0].article.image}
+                      alt="background"
+                      secondParagraph={dataAboutPage[0].article.secondParagraph}
+                  />
+              </div>
+          </section>
 
-        <section>
-            <div className="container">
-                <TitlePage name="ABOUT" marginLeft="91px" />
-            </div>
-        </section>
-        <section>
-            <div className="container">
-                <div style={{marginTop : "38px"}}></div>
-                <SmallArticle 
-                    align="center"
-                    caption = "OUR STORY"
-                    title ={dataAboutPage[0].article.title}
-                    firstParagraph ={dataAboutPage[0].article.firstParagraph}
-                    image={dataAboutPage[0].article.image}
-                    alt="background"
-                    secondParagraph={dataAboutPage[0].article.secondParagraph}
+          <section>
+              <div className="container">
+                <CardSix
+                  containerStyle={{margin: "145px 0"}}
+                  className="container"
+                  store={[
+                    {
+                      id: 1,
+                      image: dataAboutPage[0].siximage.image1,
+                      head: "Hotel"
+                    },
+                    {
+                      id: 2,
+                      image: dataAboutPage[0].siximage.image2,
+                      head: "Phinisi Cruise"
+                    },
+                    {
+                      id: 3,
+                      image: dataAboutPage[0].siximage.image3,
+                      head: "Residential"
+                    },
+                    {
+                      id: 4,
+                      image: dataAboutPage[0].siximage.image4,
+                      head: "Golf"
+                    },
+                    {
+                      id: 5,
+                      image: dataAboutPage[0].siximage.image5,
+                      head: "Restaurant"
+                    },
+                    {
+                      id: 6,
+                      image: dataAboutPage[0].siximage.image6,
+                      head: "Entertainment"
+                    }
+                  ]}
                 />
-            </div>
-        </section>
+              </div>
+            </section>
 
-        <section>
+            <section>
+              <div className="container" id="philosophy">
+                  <Title 
+                      text="Our Philosophy"
+                      image="http://timpietrusky.com/cdn/army.png"
+                      margin="92px 0px 0px 0px"
+                      
+                  />
+              </div>
+            </section>
+
+            <section>
+              <div className="container" id="imagephilosophy">
+                <CardImageLarge 
+                  caption="OUR PHILOSOPHY"
+                  title={dataAboutPage[0].philosophy.title}
+                  text={dataAboutPage[0].philosophy.text}
+                  link="#linkTo"
+                  btn={false}
+                  background={dataAboutPage[0].philosophy.image}
+                  reverse={true}
+                />
+              </div>
+            </section>
+      
+            <section>
+              <div className="container" align="right" id="sustainability">
+                <Title 
+                  text="Sustainability"
+                  image="http://timpietrusky.com/cdn/army.png"
+                  margin="79px 0px 90px 0px"
+                />
+              </div>
+            </section>
+
+            <section>
+              <div className="container" id="imagesustainability">
+                <CardImageLarge 
+                  caption="sustainability"
+                  title={dataAboutPage[0].sustainability.title}
+                  text={dataAboutPage[0].sustainability.text}
+                  link="#linkTo"
+                  btn={false}
+                  background={dataAboutPage[0].sustainability.image}
+                  reverse={false}
+                />
+              </div>
+            </section>
+
+
+            <section>
+              <footer>
+                <Footer />
+              </footer>
+            </section>
+          </OnDesktop>
+          <OnMobileAndTablet>
+            <section>
+              <div className="container">
+                <MobileCardTextSecondary 
+                  title="lorem ipsum dolor sit amet"
+                  text="From timeless icons in buzzing cities to tranquil retreats in hidden corners, The maj offers unforgettable travel experiences in pioneering destinations across the globe."
+                  margin="28px 0"
+                />
+
+                <MobileTitle 
+                  text="Destination" 
+                  image={background}
+                  margin="10px 0px 49px 0px" 
+                />
+              </div>
+            </section>      
+          </OnMobileAndTablet>  
+          <section>
             <div className="container">
-              <CardSix
-                containerStyle={{margin: "145px 0"}}
-                className="container"
+              <MobileSliderImageOverlap
                 store={[
                   {
                     id: 1,
-                    image: dataAboutPage[0].siximage.image1,
-                    head: "Hotel"
+                    source: background,
+                    name: "Hotel",
+                    description: "Esse tempor qui enim ut aute cupidatat in dolor magna irure voluptate consequat Lorem. Occaecat ad adipisicing enim Lorem minim ea elit exercitation dolor et ad consequat aliqua.",
+                    link: "#linkTo"
                   },
                   {
                     id: 2,
-                    image: dataAboutPage[0].siximage.image2,
-                    head: "Phinisi Cruise"
+                    source: background,
+                    name: "Rumah",
+                    description: "Esse tempor qui enim ut aute cupidatat in dolor magna irure voluptate consequat Lorem. Occaecat ad adipisicing enim Lorem minim ea elit exercitation dolor et ad consequat aliqua.",
+                    link: "#linkTo"
                   },
                   {
-                    id: 3,
-                    image: dataAboutPage[0].siximage.image3,
-                    head: "Residential"
-                  },
-                  {
-                    id: 4,
-                    image: dataAboutPage[0].siximage.image4,
-                    head: "Golf"
-                  },
-                  {
-                    id: 5,
-                    image: dataAboutPage[0].siximage.image5,
-                    head: "Restaurant"
-                  },
-                  {
-                    id: 6,
-                    image: dataAboutPage[0].siximage.image6,
-                    head: "Entertainment"
+                    id: 3, 
+                    source: background,
+                    name: "Restaurant",
+                    description: "Esse tempor qui enim ut aute cupidatat in dolor magna irure voluptate consequat Lorem. Occaecat ad adipisicing enim Lorem minim ea elit exercitation dolor et ad consequat aliqua.",
+                    link: "#linkTo"
                   }
                 ]}
               />
             </div>
-          </section>
-
-          <section>
-            <div className="container" id="philosophy">
-                <Title 
-                    text="Our Philosophy"
-                    image="http://timpietrusky.com/cdn/army.png"
-                    margin="92px 0px 0px 0px"
-                    
-                />
-            </div>
-          </section>
-
-          <section>
-            <div className="container" id="imagephilosophy">
-              <CardImageLarge 
-                caption="OUR PHILOSOPHY"
-                title={dataAboutPage[0].philosophy.title}
-                text={dataAboutPage[0].philosophy.text}
-                link="#linkTo"
-                btn={false}
-                background={dataAboutPage[0].philosophy.image}
-                reverse={true}
-              />
-            </div>
-          </section>
-     
-          <section>
-            <div className="container" align="right" id="sustainability">
-              <Title 
-                text="Sustainability"
-                image="http://timpietrusky.com/cdn/army.png"
-                margin="79px 0px 90px 0px"
-              />
-            </div>
-          </section>
-
-          <section>
-            <div className="container" id="imagesustainability">
-              <CardImageLarge 
-                caption="sustainability"
-                title={dataAboutPage[0].sustainability.title}
-                text={dataAboutPage[0].sustainability.text}
-                link="#linkTo"
-                btn={false}
-                background={dataAboutPage[0].sustainability.image}
-                reverse={false}
-              />
-            </div>
-          </section>
-
-
-          <section>
-            <footer>
-              <Footer />
-            </footer>
           </section>
       </div>
     )
